d8d22c74e0635c689fc2c677cbdb1511a6f3f162
Sudeepck03 <124416495+Sudeepck03@users.noreply.github.com>
2023-02-08 13:43:17 +0530

    Add files via upload

---
+++ Swiggy/SwiggyScrapper.js
@@ -0,0 +1,133 @@
+const express = require('express')
+const app = express()
+const puppeteer = require('puppeteer');
+const nodemailer = require('nodemailer');
+
+const {getLinks} = require('./Getlinks')
+
+let resturantsAndDishes = "Restuarnts are :" + " " +  "\n";
+let counter = 0;
+
+
+app.get('/dishes',async(req,res)=>{
+    let {city,restaurants,email} = req.query
+    const resturantData =await main(city,restaurants);
+    req.dishes = resturantData
+    let requiredResturantData = resturantsAndDishes
+    console.log(requiredResturantData)
+
+    let transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:"1ms19is119@gmail.com",
+        pass:"popoqibpxuxpclbr"
+    },
+    tls:{
+        rejectUnauthorized : false
+    },
+    secure : false
+})
+
+
+    let mailOptions = {
+        from :"1ms19is119@gmail.com",
+        to:email,
+        subject:"Swiggy.com",
+        text:"Firts Email Using Nodejs" +" " +  resturantData + " " ,
+        html:requiredResturantData,
+        amp:`${requiredResturantData}`,
+        secure : false,
+    }
+
+      transporter.sendMail(mailOptions,function(err,success){
+        if(err){
+          console.log(err);
+        }else{
+            console.log(success)
+        }
+      })  
+     res.send(req.dishes)
+})
+
+
+const port = 3000
+app.listen(port,()=>console.log(`Successfully connected to Servere ${port}`))
+
+
+
+async function Getdata(url,page,filteredRestuarnt) {
+      await page.setDefaultNavigationTimeout(0);
+      await page.goto(url);
+      await page.waitForSelector('[data-testid="normal-dish-item"] > div > div:first-of-type');
+
+      // Extract dish data
+      let dishes = [];
+      let dishElements = await page.$$('[data-testid="normal-dish-item"] > div > div:first-of-type');
+      const hotelName =  await page.$eval('h1',n=>n.title)
+     
+      //Apply Offer if existes
+      let BestOffer =  await page.$eval('._3lvLZ',n => n.innerText)
+      BestOffer = BestOffer.split(" ")
+      let percentage = Number(BestOffer[0].substring(0,BestOffer[0].length-1));
+      limit = Number(BestOffer[4].substring(1,BestOffer[4].length))
+      let filteredDishes =[]
+      
+    // Check if Restaurant is present in Query Filter
+      if(!filteredRestuarnt.includes(hotelName) && filteredRestuarnt.length > 0){
+         return  { [hotelName] : { dishes : filteredDishes }}
+      }
+           
+    let finalPrice = 0;
+    counter++;
+    resturantsAndDishes += counter +" ." + hotelName +":" + " " 
+      for (const dishElement of dishElements) {
+        const dishName = await dishElement.$eval('h3', node => node.textContent);
+        let dishPrice = Number(await dishElement.$eval('.rupee', node => node.textContent));
+        let discount = (dishPrice * (percentage) / 100)
+        let dishPriceAfterDiscount = discount < limit ? Number(dishPrice - discount) : Number(dishPrice - limit);
+        if(isNaN(dishPriceAfterDiscount)){
+            dishes.push({ dishName, dishPrice})
+             finalPrice += Number(dishPrice)
+             if(dishPrice >= 20 && dishPrice <= 300) resturantsAndDishes += dishName +","
+        }else{
+            dishes.push({ dishName, dishPrice: dishPriceAfterDiscount}); 
+            finalPrice += Number(dishPriceAfterDiscount)
+           if(dishPriceAfterDiscount >= 20 && dishPriceAfterDiscount <= 300) resturantsAndDishes += dishName +" ,"
+        }   
+    }
+    finalPrice = Number(finalPrice)
+   
+    resturantsAndDishes += ":" + finalPrice
+   
+      // Filter dishes within the price range
+    filteredDishes = dishes.filter(dish => dish.dishPrice >= 20 && dish.dishPrice <= 300);
+    
+      return {
+        [hotelName] : {
+          Name : hotelName,
+          dishes : filteredDishes 
+      },
+      finalPrice,
+    }
+};
+
+async function main(city,restaurants){
+        const allLinks = await getLinks(city,restaurants);
+        const browser = await puppeteer.launch({headless: false, args: ["--no-sandbox"]});
+        let page = await browser.newPage();
+
+        const arr = [];
+        let count=0;
+    
+        for(let link of allLinks.links){
+            count++;
+            if(count > 2){
+                break;
+            }
+          const data = await Getdata(link,page,allLinks.filteredRestuarnt);
+          arr.push(data)
+        }
+   
+    await browser.close()
+    return resturantsAndDishes;
+}
